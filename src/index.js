8cc06692e4fbb84c9e0d46ef82e388eca7664ce8
chonsukp <chonsukp@gmail.com>
2024-10-15 19:59:01 +1000

    feat: project rearranged for test purposes

--- src/index.js
+++ src/index.js
@@ -1,36 +1,7 @@
-const express = require("express");
+const {app} = require('./server.js');
 
-// make an instance of the express system so we can configure it eg. routes, settings 
-const app = express();
-
-// GET localhost:3000/
-// .get("/", req, res) =>{})
-// .get("/", (banana, elephat) => {})
-app.get("/", (request, response) => {
-    // response.send("<h1>Hello word!</h1>");
-    response.json({
-        message:"Hello World!"
-    })
-});
-
-app.post("/", (request, response) => {
-    response.json({
-        message:"POST request received!"
-    });
-})
-
-app.post("/bananas", (request, response) => {
-    response.json({
-        message:"POST bananas received!"
-    });
-})
-
-const PokemonController = require("./controllers/pokemonController.js");
-// localhost:3000/pokemon/
-app.use("/pokemon", PokemonController);
-
-const PORT = 3000
+const PORT = process.env.PORT ?? 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server listening to localhost:${PORT}`);
+	console.log(`Server listening on localhost:${PORT}`);
 });
