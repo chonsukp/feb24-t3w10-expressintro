ad40da31a367696c0b94a704b3a5cdc1f55b5723
chonsukp <chonsukp@gmail.com>
2024-10-15 19:32:46 +1000

    feat: other http verbs

--- src/index.js
+++ src/index.js
@@ -13,6 +13,18 @@ app.get("/", (request, response) => {
     })
 });
 
+app.post("/", (request, response) => {
+    response.json({
+        message:"POST request received!"
+    });
+})
+
+app.post("/bananas", (request, response) => {
+    response.json({
+        message:"POST bananas received!"
+    });
+})
+
 const PORT = 3000
 
 app.listen(PORT, () => {
